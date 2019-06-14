f9fa038f9a6dc387333e0d8f52577083079a073f
rocio <rocio_288@hotmail.com>
2019-06-14 14:54:29 -0300

    Los comentarios de documentación son tokens; solo se permiten en ciertos lugares en el archivo de entrada ya que pertenecen al árbol de sintaxis! Esta característica permite generadores de documentación más simples.  Los comentarios de varias líneas se inician con # [ y terminan con ] # . Los comentarios multilínea también pueden ser anidados.

---
+++ ejemplo-5b.nim
@@ -0,0 +1,8 @@
+#[
+You can have any Nim code text commented
+out inside this with no indentation restrictions.
+      yes("May I ask a pointless question?")
+  #[
+     Note: these can be nested!!
+  ]#
+]#
